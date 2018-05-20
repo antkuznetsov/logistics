d47b8424d66e0961ad08e4868509bf9bd80f6ee0
Anton Kuznetsov <me@antkuznetsov.ru>
2018-05-20 14:33:15 +0300

    Обновлены ченджсеты для разворачивания БД. Добавлена генерация данных. Исправлена проблема с кодировкой.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -16,6 +16,87 @@ public class MainTest {
         } catch (SQLException e) {
             System.out.println("Проблема с соединением с БД!");
         }
+        /*
+        System.out.println("Заполняем таблицы тестовыми данными...");
+
+        try {
+
+            //Таблица «Организация-заказчик»
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO public.organization (id, name) VALUES (1, 'ООО Бестранк'); " +
+                            "INSERT INTO public.organization (id, name) VALUES (2, 'ООО Ай-Теко');"
+            );
+
+            statement.executeQuery();
+
+            //Таблица «Товар»
+            statement = connection.prepareStatement(
+                    "INSERT INTO public.product (id, name) VALUES (1, 'iPhone'); " +
+                            "INSERT INTO public.product (id, name) VALUES (2, 'Samsung'); " +
+                            "INSERT INTO public.product (id, name) VALUES (3, 'Nokia'); " +
+                            "INSERT INTO public.product (id, name) VALUES (4, 'Xiaomi'); " +
+                            "INSERT INTO public.product (id, name) VALUES (5, 'Motorola');"
+            );
+
+            statement.executeQuery();
+
+            //Таблица «Место доставки»
+            statement = connection.prepareStatement(
+                    "INSERT INTO public.location (id, city, address) VALUES (3, 'Санкт-Петербург', 'Лиговский 54'); " +
+                            "INSERT INTO public.location (id, city, address) VALUES (1, 'Санкт-Петербург', 'Невский 151'); " +
+                            "INSERT INTO public.location (id, city, address) VALUES (2, 'Иннополис', 'Спортивная 116');"
+            );
+
+            statement.executeQuery();
+
+            //Таблица «Договор»
+            statement = connection.prepareStatement(
+                    "INSERT INTO public.contract (id, amount, details, organization_id) VALUES (1, 50000, 'Тест', 1); " +
+                            "INSERT INTO public.contract (id, amount, details, organization_id) VALUES (2, 10000, 'Тест 2', 2); " +
+                            "INSERT INTO public.contract (id, amount, details, organization_id) VALUES (3, 500, 'Тест 3', 1);"
+            );
+
+            statement.executeQuery();
+
+            //Таблица «Платеж по договору»
+            statement = connection.prepareStatement(
+                    "INSERT INTO public.payment (id, type, date, amount, contract_id) VALUES (1, '', '', 25000, 1); " +
+                            "INSERT INTO public.payment (id, type, date, amount, contract_id) VALUES (3, '', '', 1000, 2); " +
+                            "INSERT INTO public.payment (id, type, date, amount, contract_id) VALUES (4, '', '', 2700, 1); " +
+                            "INSERT INTO public.payment (id, type, date, amount, contract_id) VALUES (5, '', '', 2300, 2); " +
+                            "INSERT INTO public.payment (id, type, date, amount, contract_id) VALUES (2, '', '', 500, 3);"
+            );
+
+            statement.executeQuery();
+
+            //Таблица «Доставка»
+            statement = connection.prepareStatement(
+                    "INSERT INTO public.shipment (id, s_date_expected, s_date_actual, location_id, r_date_expected, r_date_actual, contract_id) VALUES (4, null, '2018-05-01', 3, null, '2018-05-17', 2); " +
+                            "INSERT INTO public.shipment (id, s_date_expected, s_date_actual, location_id, r_date_expected, r_date_actual, contract_id) VALUES (5, null, '2018-05-01', 2, null, '2018-05-02', 2); " +
+                            "INSERT INTO public.shipment (id, s_date_expected, s_date_actual, location_id, r_date_expected, r_date_actual, contract_id) VALUES (1, null, '2018-05-14', 1, null, '2018-05-17', 1); " +
+                            "INSERT INTO public.shipment (id, s_date_expected, s_date_actual, location_id, r_date_expected, r_date_actual, contract_id) VALUES (2, null, '2018-04-25', 2, null, '2018-04-30', 1); " +
+                            "INSERT INTO public.shipment (id, s_date_expected, s_date_actual, location_id, r_date_expected, r_date_actual, contract_id) VALUES (3, null, '2018-05-01', 1, null, '2018-05-03', 1); " +
+                            "INSERT INTO public.shipment (id, s_date_expected, s_date_actual, location_id, r_date_expected, r_date_actual, contract_id) VALUES (6, null, null, 1, null, null, 1);"
+            );
+
+            statement.executeQuery();
+
+            //Таблица «Доставка-Товар»
+            statement = connection.prepareStatement(
+                    "INSERT INTO public.shipment__product (shipment_id, product_id, count) VALUES (1, 1, 2); " +
+                            "INSERT INTO public.shipment__product (shipment_id, product_id, count) VALUES (1, 2, 1); " +
+                            "INSERT INTO public.shipment__product (shipment_id, product_id, count) VALUES (1, 3, 4); " +
+                            "INSERT INTO public.shipment__product (shipment_id, product_id, count) VALUES (2, 1, 4); " +
+                            "INSERT INTO public.shipment__product (shipment_id, product_id, count) VALUES (3, 1, 1);"
+            );
+
+            statement.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Заполнение окончено!");*/
     }
 
     @Test
